c37db3358fd7dde51970335fda6503a9bea922b1
Jess Lê <jessicale90@gmail.com>
2023-08-07 14:59:54 -0600

    Disable DarkMode

--- MeetMe/Map/MeetXViewController.swift
+++ MeetMe/Map/MeetXViewController.swift
@@ -11,7 +11,7 @@ class MeetXViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .systemGray
     }
 
 }
