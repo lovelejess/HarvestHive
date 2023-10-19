20a2b05e549f6db831a5858f1d4472937f254aed
Jess Lê <jessicale90@gmail.com>
2023-10-18 21:42:53 -0600

    Removes dead code

--- HarvestHive/Map/MapView.swift
+++ HarvestHive/Map/MapView.swift
@@ -27,32 +27,6 @@ struct MapView: View {
             )
         }
     }
-//        switch viewModel.state {
-//        case .idle:
-//            // Render a clear color and start the loading process
-//            // when the view first appears, which should make the
-//            // view model transition into its loading state:
-//            Color.clear.onAppear(perform: viewModel.fetchLocation)
-//        case .loading:
-//            ProgressView()
-//        case .failed(let error):
-//            Map(coordinateRegion: $viewModel.region,
-//                interactionModes: .all,
-//                showsUserLocation: true,
-//                userTrackingMode: $tracking)
-//            .alert(isPresented: $viewModel.shouldShowErrorAlert) {
-//                Alert(
-//                    title: Text("Current Location Not Available"),
-//                    message: Text("Cannot retrieve Hives: \(error.localizedDescription). Please update privacy in Settings > Privacy > Location > App.")
-//                )
-//            }
-//        case .loaded:
-//            Map(coordinateRegion: $viewModel.region,
-//                interactionModes: .all,
-//                showsUserLocation: true,
-//                userTrackingMode: $tracking)
-//        }
-//    }
 }
 
 struct MapView_Previews: PreviewProvider {
